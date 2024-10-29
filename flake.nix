c1ba1fa8b61443b0ed0f0c57746b7b506d522585
zzucch <nikitamilovatskiy@gmail.com>
2024-10-29 22:51:05 +0300

    build: use naersk

--- flake.nix
+++ flake.nix
@@ -1,37 +1,37 @@
 {
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   outputs = {
     self,
+    flake-utils,
+    naersk,
     nixpkgs,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        });
-  in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-          rustPackages.clippy
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        naersk' = pkgs.callPackage naersk {};
+      in {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
 
-          openssl
-          pkg-config
-        ];
-      };
-    });
-  };
+              openssl
+              pkg-config
+            ];
+          };
+      }
+    );
 }
